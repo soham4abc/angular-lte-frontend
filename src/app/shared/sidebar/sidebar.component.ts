709af20b3c4823a333fb3b14fd12a88f6cafbc69
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2020-11-11 11:50:21 +0530

    Routes on sidebar fixed with addcategories

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -14,9 +14,32 @@ export class SidebarComponent implements OnInit {
         // });
 
     }
-    routeNew() {
+    routeCat() {
         console.log('test');
-        window.location.href = '/layout/addcategory';
+        window.location.href = '/layout/categorymanagement';
+        // this.router.navigateByUrl('/layout/employees');
+    }
+    routeDash() {
+        console.log('test');
+        window.location.href = '/layout/dashboard';
+        // this.router.navigateByUrl('/layout/employees');
+    }
+    routeUser() {
+        console.log('test');
+        window.location.href = '/layout/profile/user';
+        // this.router.navigateByUrl('/layout/employees');
+    }
+    routeCom() {
+        console.log('test');
+        window.location.href = '/layout/profile/company';
+        // this.router.navigateByUrl('/layout/employees');
+    }routePswd() {
+        console.log('test');
+        window.location.href = '/layout/profile/update-password';
+        // this.router.navigateByUrl('/layout/employees');
+    }routeOut() {
+        console.log('test');
+        window.location.href = '/login';
         // this.router.navigateByUrl('/layout/employees');
     }
 }
